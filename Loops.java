35f406bf5ca149dd05ef960d898f1ee5dfe2be18
Mukul Bhardwaj <127861813+MukulB0412@users.noreply.github.com>
2023-08-21 23:24:15 +0530

    loops

---
+++ Loops.java
@@ -0,0 +1,14 @@
+package com.mukul;
+
+public class Loops {
+    public static void main(String[] args) {
+        //Loops make it easy for us to tell the computer that a given set of instructions need to be executed repeatedly.
+
+        //While loops
+        int i = 1;
+        while (i<=1000){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
