385ee39e3e0f9c36a1383706380f1be6cdbf392a
Mukul Bhardwaj <127861813+MukulB0412@users.noreply.github.com>
2023-08-23 00:46:59 +0530

    forloops

---
+++ forLoops.java
@@ -0,0 +1,18 @@
+package com.mukul;
+
+public class forLoops {
+    public static void main(String[] args) {
+//        for (int i = 1; i < 10; i++) {
+//            System.out.println(i);
+//        }
+
+
+//        for ( int i = 0; i <100 ; i++){
+//            System.out.println(2*i+1);
+//        }
+
+        for (int i=45678; i>0; i--){
+            System.out.println(i);
+        }
+    }
+}
