9f6f148933d5ecbe1eae56b9a4db5279bc9f1655
Mukul Bhardwaj <127861813+MukulB0412@users.noreply.github.com>
2023-10-07 21:39:58 +0530

    Add files via upload

---
+++ Inheritance.java
@@ -0,0 +1,19 @@
+package com.mukul;
+
+class Base{
+    int x;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void printMe(){
+        System.out.println("I am a constructor");
+    }
+}
+public class Inheritance {
+}
